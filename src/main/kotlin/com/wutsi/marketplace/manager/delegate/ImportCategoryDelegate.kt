41e1dabc458e90e9081a928bf86604a4851fac54
wutsi <herve.tchepannou@wutsi.com>
2022-11-10 17:40:40 -0500

    import categories

---
+++ src/main/kotlin/com/wutsi/marketplace/manager/delegate/ImportCategoryDelegate.kt
@@ -0,0 +1,14 @@
+package com.wutsi.marketplace.manager.`delegate`
+
+import com.wutsi.marketplace.manager.workflow.category.ImportCategoryWorkflow
+import com.wutsi.workflow.WorkflowContext
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Service
+
+@Service
+public class ImportCategoryDelegate(private val workflow: ImportCategoryWorkflow) {
+    @Async
+    public fun invoke(language: String) {
+        workflow.execute(WorkflowContext(language))
+    }
+}
